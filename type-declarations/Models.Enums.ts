b48dce4ed95995d4bc15eabccffd58a686a7b9f1
Steve Greatrex <stephen.greatrex@googlemail.com>
2016-12-05 08:53:27 +0000

    Added e10 d.ts definitions

---
+++ type-declarations/Models.Enums.ts
@@ -0,0 +1,358 @@
+﻿module System {
+    export const enum DayOfWeek {
+        Sunday = 0,
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6
+    }
+}
+module Api.Models.Jobs {
+    export const enum JobState {
+        Pending = 0,
+        Running = 1,
+        Complete = 2,
+        AwaitingUserInput = 3,
+        Error = 4,
+        AwaitingExternal = 5,
+        Paused = 6
+    }
+
+    export const enum ScheduleRepeatType {
+        Hour  = 0,
+        Day   = 1,
+        Week  = 2,
+        Month = 3
+    }
+}
+module System.Reflection {
+    export const enum MemberTypes {
+        Constructor = 1,
+        Event = 2,
+        Field = 4,
+        Method = 8,
+        Property = 16,
+        TypeInfo = 32,
+        Custom = 64,
+        NestedType = 128,
+        All = 191
+    }
+    export const enum ParameterAttributes {
+        None = 0,
+        In = 1,
+        Out = 2,
+        Lcid = 4,
+        Retval = 8,
+        Optional = 16,
+        ReservedMask = 61440,
+        HasDefault = 4096,
+        HasFieldMarshal = 8192,
+        Reserved3 = 16384,
+        Reserved4 = 32768
+    }
+    export const enum EventAttributes {
+        None = 0,
+        SpecialName = 512,
+        ReservedMask = 1024,
+        RTSpecialName = 1024
+    }
+    export const enum FieldAttributes {
+        FieldAccessMask = 7,
+        PrivateScope = 0,
+        Private = 1,
+        FamANDAssem = 2,
+        Assembly = 3,
+        Family = 4,
+        FamORAssem = 5,
+        Public = 6,
+        Static = 16,
+        InitOnly = 32,
+        Literal = 64,
+        NotSerialized = 128,
+        SpecialName = 512,
+        PinvokeImpl = 8192,
+        ReservedMask = 38144,
+        RTSpecialName = 1024,
+        HasFieldMarshal = 4096,
+        HasDefault = 32768,
+        HasFieldRVA = 256
+    }
+    export const enum PropertyAttributes {
+        None = 0,
+        SpecialName = 512,
+        ReservedMask = 62464,
+        RTSpecialName = 1024,
+        HasDefault = 4096,
+        Reserved2 = 8192,
+        Reserved3 = 16384,
+        Reserved4 = 32768
+    }
+    export const enum TypeAttributes {
+        VisibilityMask = 7,
+        NotPublic = 0,
+        Public = 1,
+        NestedPublic = 2,
+        NestedPrivate = 3,
+        NestedFamily = 4,
+        NestedAssembly = 5,
+        NestedFamANDAssem = 6,
+        NestedFamORAssem = 7,
+        LayoutMask = 24,
+        AutoLayout = 0,
+        SequentialLayout = 8,
+        ExplicitLayout = 16,
+        ClassSemanticsMask = 32,
+        Class = 0,
+        Interface = 32,
+        Abstract = 128,
+        Sealed = 256,
+        SpecialName = 1024,
+        Import = 4096,
+        Serializable = 8192,
+        WindowsRuntime = 16384,
+        StringFormatMask = 196608,
+        AnsiClass = 0,
+        UnicodeClass = 65536,
+        AutoClass = 131072,
+        CustomFormatClass = 196608,
+        CustomFormatMask = 12582912,
+        BeforeFieldInit = 1048576,
+        ReservedMask = 264192,
+        RTSpecialName = 2048,
+        HasSecurity = 262144
+    }
+    export const enum GenericParameterAttributes {
+        None = 0,
+        VarianceMask = 3,
+        Covariant = 1,
+        Contravariant = 2,
+        SpecialConstraintMask = 28,
+        ReferenceTypeConstraint = 4,
+        NotNullableValueTypeConstraint = 8,
+        DefaultConstructorConstraint = 16
+    }
+    export const enum MethodImplAttributes {
+        CodeTypeMask = 3,
+        IL = 0,
+        Native = 1,
+        OPTIL = 2,
+        Runtime = 3,
+        ManagedMask = 4,
+        Unmanaged = 4,
+        Managed = 0,
+        ForwardRef = 16,
+        PreserveSig = 128,
+        InternalCall = 4096,
+        Synchronized = 32,
+        NoInlining = 8,
+        AggressiveInlining = 256,
+        NoOptimization = 64,
+        MaxMethodImplVal = 65535
+    }
+    export const enum MethodAttributes {
+        MemberAccessMask = 7,
+        PrivateScope = 0,
+        Private = 1,
+        FamANDAssem = 2,
+        Assembly = 3,
+        Family = 4,
+        FamORAssem = 5,
+        Public = 6,
+        Static = 16,
+        Final = 32,
+        Virtual = 64,
+        HideBySig = 128,
+        CheckAccessOnOverride = 512,
+        VtableLayoutMask = 256,
+        ReuseSlot = 0,
+        NewSlot = 256,
+        Abstract = 1024,
+        SpecialName = 2048,
+        PinvokeImpl = 8192,
+        UnmanagedExport = 8,
+        RTSpecialName = 4096,
+        ReservedMask = 53248,
+        HasSecurity = 16384,
+        RequireSecObject = 32768
+    }
+    export const enum CallingConventions {
+        Standard = 1,
+        VarArgs = 2,
+        Any = 3,
+        HasThis = 32,
+        ExplicitThis = 64
+    }
+}
+module System.Runtime.InteropServices {
+    export const enum LayoutKind {
+        Sequential = 0,
+        Explicit = 2,
+        Auto = 3
+    }
+}
+module Api.Models.Comments {
+    export const enum CommentVisibility {
+        Everyone = 0,
+        MentionsOnly = 1,
+        Explicit = 2
+    }
+}
+module Api.Models.Enums {
+    export const enum ContactType {
+        Unknown = 0,
+        Subject = 1,
+        User = 2
+    }
+    export const enum AccessLevel {
+        Standard = 0,
+        Admin = 1,
+        Super = 2,
+        System = 3
+    }
+    export const enum AttachmentType {
+        File = 0,
+        Uri = 1,
+        External = 2
+    }
+    export const enum AttributeType {
+        Numeric = 0,
+        Text = 1,
+        Bool = 2,
+        SingleLookup = 3,
+        MultiLookup = 4,
+        Table = 5,
+        Exercise = 6,
+        Duration = 7,
+        DateTime = 8
+    }
+    export const enum ExerciseType {
+        WeightsBased = 0,
+        DurationBased = 1
+    }
+    export const enum CalculatedAttributeScope {
+        CurrentOnly = 0,
+        CurrentAndGeneral = 1,
+        EntireSession = 2
+    }
+    export const enum PermissionLevel {
+        None = 0,
+        ViewSummary = 1,
+        View = 2,
+        Edit = 3,
+        EditDefinition = 4
+    }
+	export const enum RecordPermissionType {
+		Everyone = 0,
+		Myself = 1,
+		DistributionLists = 2
+	}
+    export const enum ChangeType {
+        Worse = -1,
+        NoChange = 0,
+        Better = 1
+    }
+    export const enum TrafficLightStatus {
+        None = -1,
+        Green = 0,
+        Amber = 1,
+        Red = 2
+    }
+    export const enum QueryEntityType {
+        Subject = 0,
+        Group = 1,
+        User = 2,
+        AllSubjects = 3,
+        AllUsers = 4
+    }
+    export const enum QueryDateRangeType {
+        CalendarWeek = 0,
+        CalendarMonth = 1,
+        CalendarYear = 2,
+        Offset = 3,
+        Explicit = 4,
+        PreviousCalendarWeek = 5,
+        PreviousCalendarMonth = 6,
+        PreviousCalendarYear = 7,
+        Today = 8,
+        Yesterday = 9
+    }
+    export const enum GapStatus {
+        Open = 0,
+        Closed = 1
+    }
+    export const enum AggregationType {
+        None = 0,
+        Count = 1,
+        Average = 2,
+        Sum = 3,
+        Min = 4,
+        Max = 5,
+        Latest = 6,
+        Earliest = 7,
+        ValueCount = 8,
+        LatestDate = 9,
+        EarliestDate = 10,
+        StandardDeviation = 11,
+        ZScore = 12
+    }
+    export const enum QueryType {
+        MultiSessionSingleEntity = 0,
+        MultiSessionSingleAttribute = 1,
+        SingleSessionSingleEntity = 2,
+        SingleSessionMultiEntity = 3,
+        AggregationMultiEntity = 4,
+        AggregationSingleEntity = 5
+    }
+    export const enum SessionEventType {
+        Create = 0,
+        Update = 1,
+        Delete = 2
+    }
+    export const enum SignedItemType {
+      Contact = 0,
+      DataItem = 1,
+      Attachment = 2
+    }
+}
+module System.Security {
+    export const enum SecurityRuleSet {
+        None = 0,
+        Level1 = 1,
+        Level2 = 2
+    }
+}
+
+module Web.Models {
+    export const enum SessionFormat {
+        Head = 0,
+        Table = 1
+    }
+
+    export const enum SessionMode {
+        View = 0,
+        Edit = 1,
+        ReadOnly = 2
+    }
+}
+
+module DataMining.Queries {
+    export const enum ImplicitQueryElementType {
+        Template = 0,
+        SquadSummary = 1,
+        DataItem = 2
+    }
+}
+
+module Charting {
+    export const enum SeriesType {
+        Bar = 0,
+        Line = 1,
+        StepLine = 2,
+        Radar = 3,
+        Scatter = 4,
+        StackedBar = 5,
+        Pie = 6
+    }
+}
---
+++ type-declarations/Models.d.ts
@@ -0,0 +1,657 @@
+﻿/// <reference path="Models.Enums.ts" />
+
+declare module Api.Models {
+	interface IActivity {
+		activityDefinition: IActivityDefinition;
+		duration: string;
+		comments: string;
+		userIds: string[];
+		subjectIds: string[];
+		actualDuration: string;
+		rpe: number;
+		load: number;
+	}
+
+	interface IActivityDefinition {
+		id: string;
+		name: string;
+		code: string;
+		defaultDuration: string;
+		categoryIds: string[];
+		equipment: string;
+		objectives: string;
+	}
+
+	interface IDistributionList {
+		id: string;
+		name: string;
+		subjects: ISubject[];
+		users: IUser[];
+	}
+
+	interface ISubject extends IContact {
+		contactType: Models.Enums.ContactType;
+		dateOfBirth: Date;
+		title: string;
+		gender: string;
+		profession: string;
+		groupIds: string[];
+		address: string;
+		city: string;
+		region: string;
+		postcode: string;
+		country: string;
+	}
+
+	interface IContact {
+		id: string;
+		contactType: Models.Enums.ContactType;
+		firstName: string;
+		lastName: string;
+		emailAddress: string;
+		mobileNumber: string;
+		username: string;
+	}
+
+	interface IUser extends IContact {
+		contactType: Models.Enums.ContactType;
+		accessLevel: Models.Enums.AccessLevel;
+	}
+
+	interface IActivityDefinitionResults {
+		categories: ICategoryMetadata[];
+		activityDefinitions: IActivityDefinition[];
+		matchedActivityDefinitions: number;
+	}
+
+	interface ICategoryMetadata {
+		category: ICategory;
+		activityCount: number;
+	}
+
+	interface ICategory {
+		id: string;
+		name: string;
+	}
+
+	interface IAssessment {
+		id: string;
+		contactId: string;
+		contactType: Models.Enums.ContactType;
+		creatorId: string;
+		gapId: string;
+		name: string;
+		date: Date;
+		instructions: string;
+		subjectComments: string;
+		coachComments: string;
+		attachmentComments: string;
+		level: ILookupValue;
+		change: Models.Enums.ChangeType;
+		status: Models.Enums.TrafficLightStatus;
+		users: IUser[];
+		attachments: IAttachment[];
+		removedData: IRemovedSessionData;
+		removedUserIds: string[];
+		additionalData: IAttributeValue[];
+	}
+
+	interface ILookupValue {
+		id: string;
+		value: string;
+		index: number;
+		score: number;
+	}
+
+	interface IAttachment {
+		id: string;
+		description: string;
+		uri: string;
+		isAvailable: boolean;
+		type: Models.Enums.AttachmentType;
+	}
+
+	interface IRemovedSessionData {
+		attributeIds: string[];
+		attachmentIds: string[];
+		uriAttachments: string[];
+	}
+
+	interface IAttributeValue {
+		attributeDefinitionId?: string;
+		attributeDefinition?: IAttributeDefinition;
+		value: any;
+		tableRows?: IAttributeValue[][];
+		lookupValues?: ILookupValue[];
+		isDynamic?: boolean;
+	}
+
+	interface IAttributeDefinition {
+		id: string;
+		name: string;
+		description?: string;
+		type: Models.Enums.AttributeType;
+		maxValue?: number;
+		minValue?: number;
+		columnWidth?: number;
+		lookup?: ILookupDefinition;
+		tableColumns?: IAttributeDefinition[];
+		exerciseType?: Models.Enums.ExerciseType;
+		units?: string;
+		minLines?: number;
+		isCalculated?: boolean;
+		dependantAttributes?: string[];
+		script?: string;
+		scriptScope?: Models.Enums.CalculatedAttributeScope;
+		permissionLevel?: Models.Enums.PermissionLevel;
+		defaultValue?: any;
+		decimalPlaces?: number;
+		displayTemplate?: string;
+		snapshotAggregationId?: string;
+	}
+
+	interface ILookupDefinition {
+		id: string;
+		name: string;
+		values: ILookupValue[];
+	}
+
+	interface IAttributeScoreBands {
+		preferLow: boolean;
+		scoreBands: IScoreBand[];
+	}
+
+	interface IScoreBand {
+		min: number;
+		score: number;
+		isMinimumStandard: boolean;
+	}
+
+	interface IFilterParameters {
+		entityId: string;
+		entityType: Models.Enums.QueryEntityType;
+		dateRangeType: Models.Enums.QueryDateRangeType;
+		date: Date;
+		dateOffset: number;
+	}
+
+	interface IGap {
+		id: string;
+		entityId: string;
+		name: string;
+		category: ICategory;
+		status: Models.Enums.GapStatus;
+		comments: string;
+		openDate: Date;
+		closeDate: Date;
+		attributeDefinitions: IAttributeDefinition[];
+		milestones: IMilestone[];
+		activities: IActivityDefinition[];
+		latestTrafficLightStatus: Models.Enums.TrafficLightStatus;
+		latestChange: Models.Enums.ChangeType;
+		lastUpdateDate: Date;
+	}
+
+	interface IMilestone {
+		date: Date;
+		value: IAttributeValue;
+	}
+
+	interface ILink {
+		type: string;
+		target: string;
+		entityId: string;
+	}
+
+	interface IQueryCell extends IQueryElementBase {
+		value: any;
+		lookupValues: ILookupValue[];
+	}
+
+	interface IQueryElementBase {
+		metadata: { [index: string]: any };
+	}
+
+	interface IQueryColumn extends IQueryElementBase {
+		header: any;
+	}
+
+	interface IQueryDefinition {
+		id: string;
+		name: string;
+		type: Models.Enums.QueryType;
+		sessionTypes: ITemplate[];
+		attributes: IAttributeDefinition[];
+		aggregations: IAggregationDefinition[];
+	}
+
+	interface ITemplate {
+		id: string;
+		name: string;
+		sessionAttributeDefinitions: IAttributeDefinition[];
+		subjectAttributeDefinitions: IAttributeDefinition[];
+		description: string;
+		permissionLevel: Models.Enums.PermissionLevel;
+		supportsActivities: boolean;
+		color: string;
+		icon: string;
+		sessionAttributesMetaData: { [attributeId: string]: ITemplateAttributeMetadata };
+		subjectAttributesMetaData: { [attributeId: string]: ITemplateAttributeMetadata };
+	}
+
+	interface ITemplateAttributeMetadata {
+		isMandatory: boolean;
+	}
+
+	interface IAggregationDefinition {
+		id: string;
+		attributeDefinition: IAttributeDefinition;
+		columnName: string;
+		template?: ITemplate;
+		type: Models.Enums.AggregationType;
+		aggregationOffset?: number;
+		valueCountName?: string;
+		startDate?: Date;
+		endDate?: Date;
+		dateOffset?: number;
+		isDynamicRange?: boolean;
+		excludeDefault?: boolean;
+		decimalPlaces?: number;
+		standardDeviationMultiplier?: number;
+	}
+
+	interface IQueryResultsAggregations {
+		max: KnockoutComputed<any[]>;
+		min: KnockoutComputed<any[]>;
+		sum: KnockoutComputed<any[]>;
+		average: KnockoutComputed<any[]>;
+		standardDeviation: KnockoutComputed<any[]>;
+		zScore: KnockoutComputed<any[]>;
+	}
+
+	interface IQueryResults extends IQueryElementBase {
+		columns: IQueryColumn[];
+		rows: IQueryRow[];
+		aggregations: IQueryResultsAggregations;
+	}
+
+	interface IQueryRow extends IQueryElementBase {
+		cells: IQueryCell[];
+	}
+
+	interface IReportDefinition {
+		id: string;
+		name: string;
+		filter: IFilterParameters;
+		createdBy: string;
+		created: Date;
+		externalAttachmentId: string;
+		viewName: string;
+		queries: IReportDefinitionQuery[];
+	}
+
+	interface IReportDefinitionQuery {
+		queryDefinition: IQueryDefinition;
+		worksheetName: string;
+		insertCellX: number;
+		insertCellY: number;
+		filter: IFilterParameters;
+		viewName: string;
+	}
+
+	interface IScoreTest {
+		definition: IScoreTestDefinition;
+		level: IScoreTestLevel;
+		minimumStandard: number;
+		categories: IScoreTestCategory[];
+	}
+
+	interface IScoreTestDefinition {
+		id: string;
+		name: string;
+	}
+
+	interface IScoreTestLevel {
+		id: string;
+		name: string;
+		index: number;
+	}
+
+	interface IScoreTestCategory {
+		id: string;
+		name: string;
+		aggregations: IScoreTestAggregation[];
+		weight: number;
+	}
+
+	interface IScoreTestAggregation {
+		id: string;
+		attributeDefinition: IAttributeDefinition;
+		aggregationType: Models.Enums.AggregationType;
+		weight: number;
+	}
+
+	interface IScoreTestAggregationResult {
+		value: any;
+		score: number;
+		isOfStandard: boolean;
+	}
+
+	interface IScoreTestCategoryResult {
+		score: number;
+		aggregationResults: { [key: string]: IScoreTestAggregationResult };
+		anyResultsNotOfStandard: boolean;
+	}
+
+	interface IScoreTestEntityResult {
+		contact: IContact;
+		score: number;
+		isOfStandard: boolean;
+		categoryResults: IScoreTestCategoryResult[];
+		anyResultsNotOfStandard: boolean;
+	}
+
+	interface IScoreTestResult {
+		scoreTest: IScoreTest;
+		entityResults: IScoreTestEntityResult[];
+	}
+
+	interface ISessionEventHandlerDefinition {
+		id: string;
+		name: string;
+		handlerName: string;
+		handlerParameters: any;
+		eventTypes: Models.Enums.SessionEventType[];
+		sessionTypeIds: string[];
+		hints: string[];
+	}
+
+	interface ISessionQueryFilters {
+		sessionIds: string[];
+		sessionLinks: ILink[];
+		contactIds: string[];
+	}
+
+	interface ISchedule {
+		until: Date;
+		days: System.DayOfWeek[];
+	}
+
+	interface ISquadSummaryColumn {
+		id: string;
+		systemAttributeDefinition: IAttributeDefinition;
+		aggregationDefinition: IAggregationDefinition;
+	}
+
+	interface ISquadSummary {
+		id: string;
+		name: string;
+		squadSummaryColumns: ISquadSummaryColumn[];
+	}
+
+	interface IContactSessionData {
+		contactId: string;
+		contactType: Models.Enums.ContactType;
+		data: IAttributeValue[];
+		attachments: IAttachment[];
+		removedData: IRemovedSessionData;
+	}
+
+	interface IGroup {
+		id: string;
+		name: string;
+		description: string;
+		singleAssignments: boolean;
+		subjects: ISubject[];
+		users: IUser[];
+		permissionLevel: Models.Enums.PermissionLevel;
+	}
+
+	interface IRemovedSessionContent {
+		contactIds: string[];
+		groupIds: string[];
+		data: IRemovedSessionData;
+	}
+
+	interface ISignature {
+		contact: IContact;
+		date: Date;
+		signedItems: ISignedItem[];
+	}
+
+	interface ISignedItem {
+		itemId: string;
+		type: Models.Enums.SignedItemType;
+		contactId?: string;
+	}
+
+	interface ISessionData extends ISessionDataBase {
+		sessionDetails: ISessionDetails;
+	}
+
+	interface ISessionDataBase {
+		data: IAttributeValue[];
+		attachments: IAttachment[];
+		contacts: IContactSessionData[];
+		groupIds: string[];
+		removedContent: IRemovedSessionContent;
+		activities: IActivity[];
+		links: ILink[];
+		signatures: ISignature[];
+	}
+
+	interface ISessionDetails {
+		id: string;
+		name: string;
+		start: Date;
+		end: Date;
+		isAllDay: boolean;
+		reminderProvided: boolean;
+		reminder: Date;
+		dueProvided: boolean;
+		due: Date;
+		lastUpdatedById: string;
+		lastUpdated: Date;
+		creatorId: string;
+		sessionTypeId: string;
+	}
+
+	interface INewSessionData extends ISessionDataBase {
+		sessionDetails: INewSessionDetails;
+	}
+
+	interface INewSessionDetails extends ISessionDetails {
+		name: string;
+		start: Date;
+		end: Date;
+		sessionTypeId: string;
+	}
+
+	interface ISessionSummary {
+		sessionDetails: ISessionDetails;
+		templateId: string;
+		templateName: string;
+		templateColor: string;
+		templateIcon: string;
+		permissionLevel: Models.Enums.PermissionLevel;
+		isSigned: boolean;
+	}
+
+	interface IDashboard {
+		dashboardName: string;
+		storageItemId: string;
+		deleted: boolean;
+		sharedNowViewingEntityFilter: Models.Enums.QueryEntityType;
+		name: string;
+		viewParameters: IFilterParameters,
+		category: ICategory;
+		owner: string;
+		createdBy: IContact;
+	}
+}
+
+declare module Api.Models.Jobs {
+	interface IJob {
+		id: string;
+		state: Jobs.JobState;
+		data: any;
+		parameters: any;
+		createdDate: Date;
+		currentStage: string;
+		createdSessionIds: string[];
+		error: any;
+		jobDefinitionName: string;
+	}
+
+	interface IJobGeneric<TData, TParameters> extends IJob {
+		data: TData;
+		parameters: TParameters;
+	}
+
+	interface IScheduleDetails {
+		startAt: Date;
+		repeatType: Api.Models.Jobs.ScheduleRepeatType;
+		repeatInterval: number;
+	}
+
+	interface IScheduleDefinition {
+		id :string;
+		status: number;
+		scheduleDetails?: IScheduleDetails;
+	}
+}
+
+declare module Api.Models.Permissions {
+	interface IDistributionListPermission {
+		distributionList: IDistributionList;
+		permissionLevel: Models.Enums.PermissionLevel;
+	}
+
+	interface IRecordPermissionSettingDetail {
+		permissionType: Models.Enums.RecordPermissionType;
+		distributionListPermissions: Array<IDistributionListPermission>;
+	}
+}
+
+declare module Api.Models.Comments {
+	interface ICommentPermissions {
+		visibility: Comments.CommentVisibility;
+		distributionLists: string[];
+	}
+
+	interface ICommentActivity {
+		date: Date;
+		description: string;
+		thread: ICommentThread;
+		authors: IContact[];
+	}
+
+	interface ICommentThread {
+		id: string;
+		title: string;
+		link: string;
+		target: string;
+		targetContact: IContact;
+		participants: IContact[];
+		comments: IComment[];
+	}
+
+	interface IComment extends ICommentPermissions {
+		id: string;
+		author: IContact;
+		body: string;
+		date: Date;
+		isUnread: boolean;
+		mentions: IContact[];
+		trafficLight: Models.Enums.TrafficLightStatus;
+		deletedBy: IContact;
+		sendEmail: boolean;
+		sendSms: boolean;
+	}
+}
+
+declare module Api.Models.Comparison {
+  interface IPropertyDifference {
+    propertyName: string;
+    oldValue: any;
+    newValue: any;
+  }
+
+  interface ICollectionDifference<TChild, TChildDifference> {
+    added: TChild[];
+    removed: TChild[];
+    changed: TChildDifference[];
+  }
+
+  interface IAttributeValueDifference {
+    oldValue: IAttributeValue;
+    newValue: IAttributeValue;
+  }
+
+	interface IDataItemsDifference extends ICollectionDifference<IAttributeValue, IAttributeValueDifference> {
+	} 
+
+  interface IContactDifference {
+    contactId: string;
+    contactType: Api.Models.Enums.ContactType;
+    values: IDataItemsDifference;
+  }
+
+  interface IContactsDifference extends ICollectionDifference<IContactSessionData, IContactDifference> {
+    addedContactDetails: IContact[];
+  }
+
+  interface ISessionDifference {
+    properties: IPropertyDifference[];
+    sessionValues: IDataItemsDifference;
+    contacts: IContactsDifference;
+    changedBy: IContact;
+    changedOn: Date;
+  }
+}
+
+declare module Api.Models.Events {
+	
+	interface ISessionEventData {
+		sessionId: string;
+		sessionTypeId: string;
+		tenant: string;
+		eventType: Api.Models.Enums.SessionEventType;
+		dateTime: Date;
+		contactId: string;
+		hints: string[];
+		updatedContacts: string[];
+		updatedDataItems: IDataItemChange[];
+	}
+
+	interface IDataItemChange {
+		dataItemId: string;
+		contactId?: string;
+	}
+}
+
+declare module Model {
+	interface IFilterParameters {
+		entityId?: string;
+		entityType?: Api.Models.Enums.QueryEntityType;
+		dateRangeType?: Api.Models.Enums.QueryDateRangeType;
+		date?: Date;
+		dateOffset?: number;
+	}
+}
+
+declare module Web.Models {
+	interface IViewElementParameters extends Model.IFilterParameters {
+		viewName?: string;
+		sessionId?: string;
+		sessionFormat?: Models.SessionFormat;
+		sessionMode?: Models.SessionMode;
+		queryName?: string;
+		implicitQueryElementId?: string;
+		implicitQueryElementType?: DataMining.Queries.ImplicitQueryElementType;
+		implicitChartType?: Charting.SeriesType;
+		chartName?: string;
+		subEntityId?: string;
+		stateId?: string;
+	}
+}
